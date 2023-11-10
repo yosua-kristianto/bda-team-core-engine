e9ca8763fa702454a73b8e3ecd648aa4aaf2a1de
yosuakristianto <yosua_kristianto144@outlook.com>
2023-11-10 18:08:58 +0700

    Added message pusher

--- ml_instance_service/src/main/handler/datalake_puller_handler.py
+++ ml_instance_service/src/main/handler/datalake_puller_handler.py
@@ -1,9 +1,11 @@
 import time;
+import os;
 from glob import glob;
 from pathlib import Path;
 from core.ml_container import predict;
 import cv2;
 from math import floor;
+from messaging.producer.image_processor_ocr_producer import image_ocr_processor_producer;
 
 """
 @todo:
@@ -22,26 +24,24 @@ def handle_datalake_puller(args: str):
 
             # Retrieve args input from python cli
             if(args == "no"):
-                # 1. Prepare the actual file path to the memory
-                # 2. Call YOLO Instance to predict the images
-                # 3. Use the bbox to crop the image
-                # 4. Save the cropped image to the post procesed folder
-
                 # Get the image from path as 
                 frame = cv2.imread(file)
 
                 # Get the filename from the path
+                file_name = os.path.basename(file);
 
+                # Do prediction
                 results = predict(frame)[0];
 
                 # Extract the boundary box
                 x, y, w, h, score, class_id = results.boxes.data[0]
 
                 # Crop the image and save the image to the post-processed folder with same name as retrieved filename 
-                cv2.imwrite("/Users/yosuakristianto/Documents/storage/post-processed/output.jpg", frame[floor(y):floor(y+h), floor(x):floor(x+w)]); 
+                final_destination = STORAGE_PATH + "/post-processed/"+ file_name;
+                cv2.imwrite(final_destination, frame[floor(y):floor(y+h), floor(x):floor(x+w)]); 
             
-
                 # Call out message broker to Tesseract to extract the text from the image. 
+                image_ocr_processor_producer(final_destination);
 
             else:
                 # 1. Prepare the actual file to Spark
--- ml_instance_service/src/main/messaging/producer/image_processor_ocr_producer.py
+++ ml_instance_service/src/main/messaging/producer/image_processor_ocr_producer.py
@@ -1,3 +1,8 @@
-"""
-IMAGE_OCR_PROCESSOR_TOPIC
-"""
+from kafka import KafkaProducer;
+import json;
+
+TOPIC = "IMAGE_OCR_PROCESSOR_TOPIC"
+
+def image_ocr_processor_producer(image_path: str): 
+    producer = KafkaProducer(bootstrap_servers="localhost:9072");
+    producer.send(TOPIC, json.dumps({"image_path": image_path}));
