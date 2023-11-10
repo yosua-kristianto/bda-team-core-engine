4be65e4a31bcd56ffa54059a21941dcf0137a3e0
Yosua Kristianto <yosua_kristianto144@outlook.com>
2023-11-10 12:34:09 +0700

    Commenting this

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-PYTHONDONTWRITEBYTECODE=1 uvicorn core_service.src.main.main:app --host 0.0.0.0 --port 8080
+# PYTHONDONTWRITEBYTECODE=1 uvicorn core_service.src.main.main:app --host 0.0.0.0 --port 8080
 
 # docker build -t your-app-name .
 # docker run -p 4000:80 your-app-name
